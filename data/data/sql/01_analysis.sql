17b5a2e17d8c825e844f8e85445a437493c3b93a
Luana Saavedra <m7m4977dw8@privaterelay.appleid.com>
2026-06-03 21:15:18 +0200

    Update 01_analysis.sql

--- data/data/sql/01_analysis.sql
+++ data/data/sql/01_analysis.sql
@@ -1,4 +1,11 @@
+-- Qual produto melhora mais o glow?
+SELECT product, AVG(glow) AS avg_glow
+FROM skincare_log
+GROUP BY product
+ORDER BY avg_glow DESC;
+
+-- Qual produto causa mais irritação?
 SELECT product, AVG(irritation) AS avg_irritation
 FROM skincare_log
 GROUP BY product
-ORDER BY avg_irritation ASC;
+ORDER BY avg_irritation DESC;
