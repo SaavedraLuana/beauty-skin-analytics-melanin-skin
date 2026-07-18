f3d8fb454e788da378ab35771b28118b18274232
Luana Saavedra <m7m4977dw8@privaterelay.appleid.com>
2026-07-18 19:51:46 +0200

    Add SQL queries for summer and winter products

---
+++ sql/03_skin_insights.sql
@@ -0,0 +1,15 @@
+-- Products used during summer
+
+SELECT
+product,
+season
+FROM products
+WHERE season='Summer';
+
+-- Products used during winter
+
+SELECT
+product,
+season
+FROM products
+WHERE season='Winter';
